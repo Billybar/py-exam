6d8786952d5d6af81035b3ba77fb49bf760cb5f9
BillyBar <barchaim10@gmail.com>
2025-07-25 19:18:48 +0300

    add new exam

--- 25b_m-81/q2.py
+++ 25b_m-81/q2.py
@@ -1,19 +1,19 @@
 def long_sub_lst(lst):
-    count , max_lan, prev = 0,0,0
+    current_seq , max_seq = 1,1
+    prev = 0
 
-    for i in range(len(lst)):
+    for i in range(1,len(lst)):
         current = lst[i]
-        if i > 0:
-            prev = lst[i-1]
+        prev = lst[i-1]
 
-        if current > 0 and prev < 0:
-            count += 1
-        elif current < 0 and prev  > 0:
-            count += 1
+        if current > 0 > prev:
+            current_seq += 1
+        elif current < 0 < prev:
+            current_seq += 1
         else:
-            count = 1
+            current_seq = 1
 
-        if count > max_lan:
-            max_lan = count
+        if current_seq > max_seq:
+            max_seq = current_seq
 
-    return max_lan
+    return max_seq
